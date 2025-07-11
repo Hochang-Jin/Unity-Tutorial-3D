9fa170105ef7a438aff3f2332a9882ca941db71e
HochangJin <hochang0115@gmail.com>
2025-07-11 10:38:51 +0900

    [ADD] Hanoi answer
    하노이 정답 보여주는 기능 추가

--- Assets/1. Data Structure/02.Scripts/Hanoi/HanoiTower.cs
+++ Assets/1. Data Structure/02.Scripts/Hanoi/HanoiTower.cs
@@ -2,22 +2,29 @@ using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HanoiTower : MonoBehaviour
 {
     public enum HanoiLevel {Lv1=3, Lv2, Lv3}
-    public HanoiLevel hanoiLevel;
+    public HanoiLevel hanoiLevel = HanoiLevel.Lv1;
 
     public static GameObject selectedDonut;
     public static bool isSelected;
     public static BoardBar currBar;
+    private bool check;
     
     public TextMeshProUGUI countText;
+    public Button answerButton;
     
     public GameObject[] donutPrefabs;
     public BoardBar[] bars;
     public static int moveCount;
-    
+
+    private void Awake()
+    {
+        answerButton.onClick.AddListener(HanoiAnswer);
+    }
 
     private IEnumerator Start()
     {
@@ -44,4 +51,34 @@ public class HanoiTower : MonoBehaviour
         
         countText.text = moveCount.ToString();
     }
+
+    private void HanoiAnswer()
+    {
+        StartCoroutine(HanoiRoutine((int)hanoiLevel, 0, 1, 2));
+    }
+
+    IEnumerator HanoiRoutine(int n, int from, int temp, int to)
+    {
+        if (n == 1)
+        {
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+            yield return new WaitForSeconds(0.5f);
+            selectedDonut = bars[from].PopDonut();
+            bars[to].PushDonut(selectedDonut);
+            
+        }
+        else
+        {
+            yield return StartCoroutine(HanoiRoutine(n-1, from, to, temp));
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+            yield return new WaitForSeconds(0.5f);
+            selectedDonut = bars[from].PopDonut();
+            bars[to].PushDonut(selectedDonut);
+            
+
+            yield return StartCoroutine(HanoiRoutine(n - 1, temp, from, to));
+        }
+        
+    }
+    
 }
