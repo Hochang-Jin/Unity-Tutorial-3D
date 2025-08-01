e4b46b00d4a5d923709ac88c49c4281570ed77f2
HochangJin <hochang0115@gmail.com>
2025-08-01 16:09:42 +0900

    0801

---
+++ Assets/4. Study/02.Scripts/StudyPartial.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public partial class StudyPartial : MonoBehaviour
+{
+    void Start()
+    {
+        MethodA();
+        MethodB();
+    }
+
+    private void MethodA()
+    {
+        
+    }
+
+   
+}
+
+public partial class StudyPartial : MonoBehaviour
+{
+    private void MethodB()
+    {
+        
+    }
+}
