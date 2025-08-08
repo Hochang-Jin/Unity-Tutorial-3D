1088f8e04429828a4844a746eb44936dae19ecc3
HochangJin <hochang0115@gmail.com>
2025-08-08 10:21:19 +0900

    [ADD] json and xml Parse

--- Assets/4. Study/02.Scripts/Parsing/JsonParser.cs
+++ Assets/4. Study/02.Scripts/Parsing/JsonParser.cs
@@ -1,6 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class JsonParser : MonoBehaviour
 {
+    [System.Serializable]
+    public class CharacterData
+    {
+        public string CharID;
+        public string Name;
+        public int HP;
+        public int Attack;
+    }
+
+    [System.Serializable]
+    public class CharacterListWrapper
+    {
+        public List<CharacterData> characters;
+    }
     
+    public List<CharacterData> characters = new List<CharacterData>();
+
+    private void Start()
+    {
+        var dataFile = Resources.Load<TextAsset>("jsonData");
+        var data = dataFile.text;
+        
+        ParsingCharacterJsonData(data);
+    }
+
+    void ParsingCharacterJsonData(string data)
+    {
+        CharacterListWrapper wrapper = JsonUtility.FromJson<CharacterListWrapper>(data);
+
+        foreach (var cData in wrapper.characters)
+        {
+            characters.Add(cData);
+            Debug.Log($"{cData.CharID} / {cData.Name}/ {cData.HP} / {cData.Attack}");
+        }
+    }
 }
--- Assets/4. Study/02.Scripts/Parsing/XMLParser.cs
+++ Assets/4. Study/02.Scripts/Parsing/XMLParser.cs
@@ -1,6 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
 
 public class XMLParser : MonoBehaviour
 {
+    [System.Serializable]
+    public class CharacterData
+    {
+        public string CharID;
+        public string Name;
+        public int HP;
+        public int Attack;
+    }
+
+    [System.Serializable]
+    [XmlRoot("Characters")]
+    public class CharacterList
+    {
+        [XmlElement("Character")]
+        public List<CharacterData> characters;
+    }
     
+    public List<CharacterData> characterDatas = new List<CharacterData>();
+
+    private void Start()
+    {
+        var dataFile = Resources.Load<TextAsset>("xmlData");
+        string data = dataFile.text;
+        
+        ParsingCharacterXmlData(data);
+    }
+
+    void ParsingCharacterXmlData(string data)
+    {
+        Debug.Log(data);
+        
+        XmlSerializer serializer = new XmlSerializer(typeof(CharacterList));
+
+        using (StringReader reader = new StringReader(data))
+        {
+            CharacterList loaded = (CharacterList)serializer.Deserialize(reader);
+            characterDatas = loaded.characters;
+        }
+
+        foreach (var cData in characterDatas)
+        {
+            Debug.Log($"{cData.CharID} / {cData.Name} / {cData.HP} / {cData.Attack}");
+        }
+    }
 }
